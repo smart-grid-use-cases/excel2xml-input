b4ae7f96299cf87a6956f6a59c5afaf98cb0f159
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2021-01-20 10:42:40 +0100

    Check release version for tag conflict
    Signed-off-by: Richard Marston <rmarston@eonerc.rwth-aachen.de>

--- get_next.sh
+++ get_next.sh
@@ -8,5 +8,17 @@ release_version=${tail%"$suffix"}
 major=`echo $release_version | cut -d. -f1`
 minor=`echo $release_version | cut -d. -f2`
 revision=`echo $release_version | cut -d. -f3`
-revision=`expr $revision + 1`
-echo ${major}.${minor}.${revision}
+found_untagged_version=false
+check_version=0.0.0
+while ! $found_untagged_version;
+do
+  revision=`expr $revision + 1`
+  check_version=${major}.${minor}.${revision}
+  check_url=https://github.com/${owner_url}/${repo_name}/releases/tag/$check_version
+  check=$(wget $check_url &> /dev/null)
+  if [ $? == 8 ];
+  then
+    found_untagged_version=true
+  fi
+done
+echo $check_version
